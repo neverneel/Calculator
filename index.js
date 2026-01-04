8af30e49368a657a8610f6ad26dd0b7eb19064e0
NEEL <neelkamalpunj@gmail.com>
2026-01-04 18:43:59 +0530

    calculator completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Calculator Program
-const display= document.getElementById("diaplay");
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value +=  input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}
