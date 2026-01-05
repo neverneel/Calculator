1b5c797db923da21f5488aa4559396f8469faa3a
NEEL <neelkamalpunj@gmail.com>
2026-01-05 20:52:39 +0530

    BackSpace Button Added

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function clearDisplay(){
     display.value = "";
 }
 
+function backSpace(){
+    display.value = display.value.slice(0, -1);
+}
+
 function calculate(){
     try{
     display.value = eval(display.value);
