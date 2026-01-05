4d8105df6f3c09a74b3b2e067d7359d516bfe30d
NEEL <neelkamalpunj@gmail.com>
2026-01-05 22:11:00 +0530

    added more key support

--- index.js
+++ index.js
@@ -9,9 +9,21 @@ function clearDisplay(){
     display.value = "";
 }
 
+document.addEventListener("keydown", (c)=>{
+    if(c.key == "Delete"){
+        clearDisplay();
+    }
+})
+
 function backSpace(){
     display.value = display.value.slice(0, -1);
 }
+
+document.addEventListener("keydown", (b)=>{
+    if(b.key === "Backspace"){
+        backSpace();
+    }
+})
 
 function calculate(){
     try{
@@ -22,8 +34,39 @@ function calculate(){
     }
 }
 
+document.addEventListener("keydown", (result)=>{
+    if(result.key === "Enter"){
+        calculate();
+    }
+})
+
 document.addEventListener("keydown", (e)=>{
     if(e.key >= "0" && e.key <= "9" ){
         appendToDisplay(e.key);
     }
 })
+
+document.addEventListener("keydown", (add)=>{
+    if(add.key == "+" ){
+        appendToDisplay(add.key);
+    }
+})
+
+document.addEventListener("keydown", (minus)=>{
+    if(minus.key == "-" ){
+        appendToDisplay(minus.key);
+    }
+})
+
+document.addEventListener("keydown", (multiply)=>{
+    if(multiply.key == "*" ){
+        appendToDisplay(multiply.key);
+    }
+})
+
+document.addEventListener("keydown", (divide)=>{
+    if(divide.key == "/" ){
+        appendToDisplay(divide.key);
+    }
+})
+
